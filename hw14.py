c2e9860f92af2511268665b07676f626adf9fead
hyejungko <44739117+hyejungko@users.noreply.github.com>
2018-11-11 21:13:13 +0900

    Update hw14.py

--- hw14.py
+++ hw14.py
@@ -1,3 +1,16 @@
-#this is python program
+mydict = dict()
+import requests
+r = requests.get('https://www.vox.com/2018/9/25/17901082/trump-un-2018-speech-full-text')
+r.encoding = 'utf8'
+data = r.text
+data = str(r.text)
+def remove_tag(content):
+    cleanr = re.compile('<.*>?')
+    cleantext = re.sub('cleanr','"','content')
+    return cleantext
+begin = data.find("President Donald Trump")
 
-print("python program")
+end = data.rfind("Thank you. (Applause.)")
+a = data[begin:end]
+
+print(a)
